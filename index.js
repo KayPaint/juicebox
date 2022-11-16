39448508b372cee37b2df1400089ba1c391db8f3
kaypaint <kelanmpainter@gmail.com>
2022-11-15 21:04:24 -0600

    11/15 exitsave

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-const PORT = 3000;
 const express = require('express');
+const morgan = require('morgan');
 const server = express();
+const apiRouter = require('./api');
 
+const PORT = 4845;
 server.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
 });
+
+server.use(morgan('dev'));
+server.use(express.json());
+
+server.use('/api', apiRouter);
 
 server.use((req, res, next) => {
     console.log("<----Body Logger START---->");
