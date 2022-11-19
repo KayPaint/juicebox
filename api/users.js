a58c5e1e2a03f16ef8ab154382754816094fb673
kaypaint <kelanmpainter@gmail.com>
2022-11-19 09:56:07 -0600

    Added api routes for /users, /posts, /tags

--- api/users.js
+++ api/users.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const usersRouter = express.Router();
 
-usersRouter.use((req, res, next) => {
-    console.log("A request is being made to /users");
-    
-    next();
-});
+const { getAllUsers } = require('../db');
 
-usersRouter.get('/', (req, res) => {
+usersRouter.get('/', async (req, res) => {
+    const users = await getAllUsers();
+
     res.send({
-        users: []
+        users
     });
 });
+
+// usersRouter.use((req, res, next) => {
+//     console.log("A request is being made to /users");
+    
+//     next();
+// });
+
+// usersRouter.get('/', (req, res) => {
+//     res.send({
+//         users: []
+//     });
+// });
 
 module.exports = usersRouter;
