09348c387e87b889e541731845847c551de8c432
kaypaint <kelanmpainter@gmail.com>
2022-11-19 10:58:36 -0600

    Added JWT

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const server = express();
+require('dotenv').config();
 const apiRouter = require('./api');
 
 // Connect client to DB
