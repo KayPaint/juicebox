f28c64522ef4c4d23a5e2f5838e325f51cb55a30
kaypaint <kelanmpainter@gmail.com>
2022-11-19 19:53:21 -0600

    Exitsave 11/19

--- api/posts.js
+++ api/posts.js
@@ -2,6 +2,11 @@ const express = require('express');
 const postsRouter = express.Router();
 
 const { getAllPosts } = require('../db');
+const { requireUser } = require('./utils');
+
+postsRouter.post('/', requireUser, async (req, res, next) => {
+    res.send({ message: 'under construction' });
+});
 
 postsRouter.get('/', async (req, res) => {
     const posts = await getAllPosts();
