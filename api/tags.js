c09669973c2915edcd4c40e1275e1df36233dccb
kaypaint <kelanmpainter@gmail.com>
2022-11-20 22:41:24 -0600

    GET /api/tags/:tagName/posts

--- api/tags.js
+++ api/tags.js
@@ -11,4 +11,18 @@ tagsRouter.get('/', async (req, res) => {
     });
 });
 
+tagsRouter.get('/:tagName/posts', async (req, res, next) => {
+    // read the tagname from the params
+    const { tagName } = req.params;
+
+    try {
+        const posts = await getPostsByTagName(tagName);
+
+        res.send({ posts });
+    } catch ({ name, message }) {
+        next({ name, message });
+    }
+});
+
+
 module.exports = tagsRouter;
