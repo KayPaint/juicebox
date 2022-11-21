f882aaf3575b9fea24d2a834c8935cb5a4d0bf08
kaypaint <kelanmpainter@gmail.com>
2022-11-20 22:36:52 -0600

    POST api/posts

--- api/posts.js
+++ api/posts.js
@@ -1,11 +1,39 @@
 const express = require('express');
 const postsRouter = express.Router();
 
-const { getAllPosts } = require('../db');
+const { getAllPosts, createPost } = require('../db');
 const { requireUser } = require('./utils');
 
 postsRouter.post('/', requireUser, async (req, res, next) => {
-    res.send({ message: 'under construction' });
+    const { title, content, tags = "" } = req.body;
+
+    const tagArr = tags.trim().split(/\s+/)
+    const postData = {};
+
+    // only send the tags if there are some to send
+    if (tagArr.length) {
+        postData.tags = tagArr;
+    }   
+
+    try {
+        postData.title = title;
+        postData.content = content;
+        postData.authorId = req.user.id;
+
+        const post = await createPost(postData);
+
+        // if the post comes back, res.send({ post });
+        if (post) {
+            res.send({ post });
+        } else {
+            next({
+                name: 'Post Error',
+                message: 'An error occured while creating the post'
+            });
+        }
+    } catch ({ name, message }) {
+        next({ name, message });
+    }
 });
 
 postsRouter.get('/', async (req, res) => {
